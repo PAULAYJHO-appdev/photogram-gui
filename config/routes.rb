9a97b21353a4f4deceec0c98a0336e2a80b861c8
Paula Ho <pho2@chicagobooth.edu>
2023-05-02 00:23:20 +0000

    Created Index Pages for Homepage, Users, and Photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+#homepage
+get("/", { :controller => "users", :action => "index"})
 
+#users
+get("/users", { :controller => "users", :action => "index"})
 
+#photos
+get("/photos", { :controller => "photos", :action => "index"})
 end
