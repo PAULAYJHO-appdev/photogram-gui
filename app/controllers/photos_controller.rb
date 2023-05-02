a3105d8f7d6a8c44b26fba2c2a7c91f971a2b215
Paula Ho <pho2@chicagobooth.edu>
2023-05-02 13:51:22 +0000

    Form to add new photo works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -25,6 +25,21 @@ def delete
 end
 
 def create
-  render({ :template => "photo_templates/create.html.erb"})
+  #Parameters: {"query_image"=>"http://www.something.com/media", "query_caption"=>"hello, it's me.", "query_owner_id"=>"117"}
+  input_image = params.fetch("query_image")
+  input_caption = params.fetch("query_caption")
+  input_owner_id = params.fetch("query_owner_id")
+
+  a_new_photo = Photo.new
+  a_new_photo.image = input_image
+  a_new_photo.caption = input_caption
+  a_new_photo.owner_id = input_owner_id
+
+  a_new_photo.save
+
+  #render({ :template => "photo_templates/create.html.erb"})
+  #instead of rendering, it might be more helpful to redirect.
+
+  redirect_to("/photos/" + a_new_photo.id.to_s)
 end
 end
