ebf2fd87e9da84280cdb35fe98c28e4f978f98ce
Paula Ho <pho2@chicagobooth.edu>
2023-05-02 11:09:01 +0000

    Image Details Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ get("/users/:path_username", { :controller => "users", :action => "show"})
 
 #photos
 get("/photos", { :controller => "photos", :action => "index"})
+get("/photos/:path_id", { :controller => "photos", :action => "show"})
 end
